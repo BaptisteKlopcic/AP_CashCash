a88bf9c82bfd92f6cc7343924d213daa57f7c25e
BaptisteKlopcic <klopcicbaptiste@gmail.com>
2026-04-29 11:43:52 +0200

    Add monthly statistics display in stats.php

---
+++ views/interventions/stats.php
@@ -0,0 +1,5 @@
+<h2>Statistiques du mois</h2>
+
+<p>Nombre d'interventions : <?= $stats['nb'] ?></p>
+<p>Kilomètres parcourus : <?= $stats['totalKm'] ?> km</p>
+<p>Durée totale : <?= $stats['totalDuree'] ?> minutes</p>
