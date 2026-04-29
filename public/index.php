c8587edb6259b848091fa4546998054333ba298b
BaptisteKlopcic <klopcicbaptiste@gmail.com>
2026-04-29 11:50:31 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,22 @@
+<?php
+session_start();
 
+$controller = $_GET['controller'] ?? 'auth';
+$action = $_GET['action'] ?? 'login';
+
+$controllerName = ucfirst($controller) . 'Controller';
+$controllerFile = "../app/controllers/" . $controllerName . ".php";
+
+if (!file_exists($controllerFile)) {
+    die("Contrôleur introuvable : $controllerName");
+}
+
+require $controllerFile;
+
+$ctrl = new $controllerName();
+
+if (!method_exists($ctrl, $action)) {
+    die("Action introuvable : $action");
+}
+
+$ctrl->$action();
