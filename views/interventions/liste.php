076b255fbd4e7ed78323eab8ca3d459262498cc8
BaptisteKlopcic <klopcicbaptiste@gmail.com>
2026-04-30 16:29:34 +0200

    Refactor intervention list table structure

--- views/interventions/liste.php
+++ views/interventions/liste.php
@@ -1,23 +1,13 @@
-<h2>Interventions</h2>
-
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Date</th>
-        <th>Client</th>
-        <th>Statut</th>
-        <th>Action</th>
-    </tr>
-
-    <?php foreach ($interventions as $i): ?>
-    <tr>
-        <td><?= $i['idIntervention'] ?></td>
-        <td><?= $i['dateIntervention'] ?></td>
-        <td><?= $i['idClient'] ?></td>
-        <td><?= $i['statut'] ?></td>
-        <td>
-            <a href="index.php?controller=intervention&action=fiche&id=<?= $i['idIntervention'] ?>">Voir</a>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-</table>
+<tbody>
+<?php foreach ($interventions as $i): ?>
+<tr>
+    <td><?= $i['client_nom'] ?></td>
+    <td><?= $i['adresse'] ?></td>
+    <td><?= $i['date'] ?></td>
+    <td><?= $i['statut'] ?></td>
+    <td>
+        <a href="/intervention/voir?id=<?= $i['id'] ?>" class="btn">Voir</a>
+    </td>
+</tr>
+<?php endforeach; ?>
+</tbody>
