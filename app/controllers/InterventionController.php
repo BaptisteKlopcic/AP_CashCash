e1885beefef24df29a75db89e7d55cdaabd4ab84
BaptisteKlopcic <klopcicbaptiste@gmail.com>
2026-04-30 16:30:00 +0200

    Update InterventionController.php

--- app/controllers/InterventionController.php
+++ app/controllers/InterventionController.php
@@ -1,13 +1,9 @@
 <?php
+
 class InterventionController {
 
     public function liste() {
-        require "../config/database.php";
-        require "../app/models/InterventionModel.php";
-
-        $model = new InterventionModel($db);
-        $interventions = $model->getAll();
-
-        require "../app/views/interventions/liste.php";
+        $interventions = Intervention::getByTechnicien($_SESSION['user']['id']);
+        require_once __DIR__ . "/../../views/interventions_liste.php";
     }
 }
