085d239727287e11bf3e46d9eb880df48c3fa500
BaptisteKlopcic <klopcicbaptiste@gmail.com>
2026-04-30 16:30:19 +0200

    Update Intervention.php

--- app/models/Intervention.php
+++ app/models/Intervention.php
@@ -1,15 +1,12 @@
 <?php
-class InterventionModel {
-    private $db;
 
-    public function __construct($db) {
-        $this->db = $db;
-    }
+class Intervention {
 
-    public function getAll() {
-        $sql = "SELECT * FROM intervention";
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute();
+    public static function getByTechnicien($idTech) {
+        $db = Database::getConnection();
+        $sql = "SELECT * FROM interventions WHERE id_technicien = ?";
+        $stmt = $db->prepare($sql);
+        $stmt->execute([$idTech]);
         return $stmt->fetchAll();
     }
 }
