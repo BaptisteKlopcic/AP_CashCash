e2e7b8b24468fc53a463d186c2f9475e28943a68
BaptisteKlopcic <klopcicbaptiste@gmail.com>
2026-06-01 09:47:45 +0200

    Enhance display of rented equipment in database.php
    Added HTML table headers and rows to display rented equipment.

--- config/database.php
+++ config/database.php
@@ -15,3 +15,21 @@ try {
 $stmt = $pdo->prepare("SELECT * FROM Materiel WHERE Etat = 'LOUE'");
 $stmt->execute();
 $materiels = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+<h1>Liste du matériel loué</h1>
+
+<tr>
+        <th>Numéro de série</th>
+        <th>Emplacement</th>
+        <th>Type</th>
+        <th>État</th>
+    </tr>
+
+    <?php foreach ($materiels as $m): ?>
+    <tr>
+        <td><?= $m['NumSerie'] ?></td>
+        <td><?= $m['Emplacement'] ?></td>
+        <td><?= $m['RefInter_TypeMateriel'] ?></td>
+        <td><?= $m['Etat'] ?></td>
+    </tr>
+    <?php endforeach; ?>
